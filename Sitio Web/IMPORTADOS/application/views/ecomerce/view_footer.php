931ac659b604bdb88e765764cc0e81df378f6dc9
Andy-20 <andi20_rukia@outlook.es>
2017-08-12 12:36:45 -0500

    Sitio Web

---
+++ Sitio Web/IMPORTADOS/application/views/ecomerce/view_footer.php
@@ -0,0 +1,24 @@
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<footer id="footer"><!--Footer-->
+		 
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Importados</p>
+					
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+	
+
+  
+    <!--<script src="<?php echo base_url()?>lib/js/jquery.js"></script>-->
+	<script src="<?php echo base_url()?>lib/js/bootstrap.min.js"></script>  
+</body>
+</html>
